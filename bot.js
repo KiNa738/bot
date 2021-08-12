c4065692208973dc74caa9e25de784c23fe70e6b
KiNa738 <baiandw738@gmail.com>
2021-08-12 17:41:03 +0300

    command add

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ bot.on("ready", () => {
     console.log(`logged in as ${bot.user.tag}`)
 })
 
+// !dog , !mimic
 bot.on("message", (message) => {
     if(message.author.bot) return
     let mess = message.content
@@ -19,16 +20,32 @@ bot.on("message", (message) => {
     let result = mess.substr(mess.indexOf(" ") + 1)
     message.channel.send(result)
 })
-// bot.on("message", (message) => {
-//     if(message.author.bot) return
-//     if (message.content == "!dog") 
-//     message.channel.send("No You")
-// })
 
-// bot.on("message", (message) => {
-//     if(message.author.bot) return
-//     if (message.content == "!dog") return;
-//     message.channel.send(message)
-// })
-
+//!add
+bot.on("message", (message) => {
+    if(message.author.bot) return
+    let mess = message.content
+    let cmd = mess.split(' ')[0];
+    let split = mess.split(' ');
+    let err = 0;
+    let x = 0;
+    if (cmd != "!add" ) return;
+    if(split.length < 3){
+        message.channel.send("دخل على الاقل رقمين");
+        return;
+    }
+    for(i = 1; i < split.length; i++){
+        if(isNaN(mess.split(' ')[i]) ){
+            err = 1;
+            break;
+        }
+        eval ('val_' + i + '= ' + mess.split(' ')[i] + ';');
+        x = x + eval('val_' + i);
+    }
+    if(err == 1){
+        message.channel.send("دخل ارقام يابيض");
+        return;
+    }
+    message.channel.send(x); 
+})
 bot.login(config.token)
